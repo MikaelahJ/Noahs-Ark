41e8a4fb36b7dad4bcd562a0f5dd64179920f435
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-09-03 18:55:07 +0200

    wave stuff

--- Assets/Scripts/Waves/Wave.cs
+++ Assets/Scripts/Waves/Wave.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Wave : MonoBehaviour
 {
+    [SerializeField] Tilemap waveTilemap;
+    [SerializeField] RuleTile waterTile;
+
     private float speed = 5f;
 
+    Vector3Int intPos;
     void Start()
     {
 
@@ -13,6 +18,18 @@ public class Wave : MonoBehaviour
 
     void Update()
     {
+        intPos = Vector3Int.FloorToInt(transform.position);
+        if (!waveTilemap.HasTile(intPos))
+        {
+            for (int i = -5; i < 5; i++)
+            {
+                Debug.Log(intPos.x + i);
+                waveTilemap.SetTile(new Vector3Int(intPos.x + i, intPos.y, intPos.z), waterTile);
+            }
+
+        }
         transform.position += transform.up * speed * Time.deltaTime;
     }
+
+
 }
